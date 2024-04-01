70aa13c5ffb491e30b46a111b22be1b49fffae6e
Kilian Naralingom <124378123+kikilian@users.noreply.github.com>
2024-04-01 03:05:11 +0200

    Delete gsbFrais directory

--- gsbFrais/views/v_accueil.php
+++
@@ -1,3 +0,0 @@
-<div id="accueil">
-    GESTION DES FRAIS DES VISITEURS
-</div>
--- gsbFrais/views/v_menu.php
+++
@@ -1,6 +0,0 @@
-<nav class="menuLeft">
-    <ul class="menu-ul">
-        <li class="menu-item"><a href="index.php">retour</a></li>
-    </ul>
-</nav>
-<section class="content">
